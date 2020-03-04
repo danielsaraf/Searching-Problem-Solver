9f8db384685fff2d9e79822e2f27f495eea5b23b
danielsaraf <57967538+danielsaraf@users.noreply.github.com>
2020-03-04 19:05:52 +0200

    Add files via upload

---
+++ ClientHandler.h
@@ -0,0 +1,13 @@
+//
+// Created by daniels on 11/01/2020.
+//
+
+#ifndef EX4_5__CLIENTHANDLER_H_
+#define EX4_5__CLIENTHANDLER_H_
+class ClientHandler {
+ public:
+  virtual void handleClient(int) = 0;
+  virtual ClientHandler* getClone() {return nullptr;};
+  virtual ~ClientHandler()= default;
+};
+#endif //EX4_5__CLIENTHANDLER_H_
---
+++ StringReverser.cpp
@@ -0,0 +1,12 @@
+//
+// Created by daniels on 11/01/2020.
+//
+
+#include <algorithm>
+#include "StringReverser.h"
+string StringReverser::solve(string str) {
+  //reverse the string
+  string reverse_str = str;
+  reverse(reverse_str.begin(), reverse_str.end());
+  return reverse_str;
+}
